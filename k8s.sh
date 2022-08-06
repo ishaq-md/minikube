54729fed0b865a2bb8a3869b40d51f2a055a1bbb
ishaqmdgcp <55760349+ishaqmdgcp@users.noreply.github.com>
2022-08-07 02:02:22 +0530

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,24 +1,29 @@
-
-#Install pre-requisites:
-#Install kubectl 
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install kubectl -y
-
-#Intsall snap
-apt-get install snap
-apt-get update
-
-
-#Intsall microk8s
+apt-get install -y snapd
 sudo snap install microk8s --classic
-microk8s status --wait-ready
-microk8s enable dashboard dns registry istio
+sudo snap install microk8s --classic --channel=1.18/stable
+sudo ufw allow in on cni0 && sudo ufw allow out on cni0
+sudo ufw default allow routed
+microk8s enable dns dashboard storage
 microk8s kubectl get all --all-namespaces
+token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
+microk8s kubectl -n kube-system describe secret $token
+microk8s kubectl -n kube-system describe secret $tokenhistory
 microk8s dashboard-proxy
 
-#Connect to dashboard via browser
-microk8s kubectl get pods
-microk8s kubectl expose pod petclinic-deployment-76ddc4c4f9-6zjps --name=petclinic-svc --port=8080 --type=NodePort
-microk8s kubectl get service
+
+
+git clone https://github.com/bibinwilson/kubernetes-prometheus
+microk8s kubectl create namespace monitoring
+cd kubernetes-prometheus/
+microk8s kubectl create -f config-map.yaml
+microk8s kubectl create -f clusterRole.yaml
+microk8s kubectl create -f prometheus-deployment.yaml
+microk8s kubectl get deployments --namespace=monitoring
+microk8s kubectl get pods --namespace=monitoring
+microk8s kubectl create -f prometheus-service.yaml --namespace=monitoring
+microk8s kubectl get service -n monitoring
+git clone https://github.com/devopscube/kube-state-metrics-configs.git
+kubectl apply -f kube-state-metrics-configs/
+microk8s kubectl apply -f kube-state-metrics-configs/
+kubectl get deployments kube-state-metrics -n kube-system
+microk8s kubectl get deployments kube-state-metrics -n kube-system
